4addd9e423bad7da41348a69383ee5761f478c20
22MH1A04I4 <119719149+22MH1A04I4@users.noreply.github.com>
2023-01-04 23:34:18 +0530

    #include<stdio.h> int main() {     int a,X,Y,Z;     scanf("%d%d%d",&X,&Y,&Z);     a=((X*5)+(Y*10))/Z;     printf("%d",a); }

---
+++ Romeo_and_Juliet.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{                                                      
+    int a,X,Y,Z;
+    scanf("%d%d%d",&X,&Y,&Z);
+    a=((X*5)+(Y*10))/Z;
+    printf("%d",a);
+}
