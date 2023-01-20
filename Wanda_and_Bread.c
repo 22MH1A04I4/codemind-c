cb7ec49343931643d273b0dc1da896a2942fa722
22MH1A04I4 <119719149+22MH1A04I4@users.noreply.github.com>
2023-01-20 10:59:02 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(b*c>=a)     {         printf("YES");     }     else if(b*c<=a)     {         printf("NO");     }     else if(b*c>=a)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(b*c>=a)
+    {
+        printf("YES");
+    }
+    else if(b*c<=a)
+    {
+        printf("NO");
+    }
+    else if(b*c>=a)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
